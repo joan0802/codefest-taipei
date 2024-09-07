a0d1b6d2c4784145228e109eb07691794401cb3d
111062123 <joan040802@gmail.com>
2024-09-07 14:20:04 +0800

    seperate tab

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,34 @@
 import ToolButton from "./ToolButton";
 
-export default function Navbar() {
+export default function Navbar({ activeTab, setActiveTab }) {
     return (
         <div className="bg-[#DBF1F5] text-white py-4 flex justify-around">
-            <ToolButton icon="🏠" text="托嬰地圖" />
-            <ToolButton icon="📝" text="育嬰問答" />
-            <ToolButton icon="📅" text="設定" />
+            {/* Map Tab */}
+            <button onClick={() => setActiveTab('map')}>
+                <ToolButton 
+                    icon="/mapIcon.svg" 
+                    text="托嬰地圖" 
+                    active={activeTab === 'map'} 
+                />
+            </button>
+
+            {/* Chatbot Tab */}
+            <button onClick={() => setActiveTab('chatbot')}>
+                <ToolButton 
+                    icon="/chatIcon.svg" 
+                    text="育嬰問答" 
+                    active={activeTab === 'chatbot'} 
+                />
+            </button>
+
+            {/* Playground Tab */}
+            <button onClick={() => setActiveTab('playground')}>
+                <ToolButton 
+                    icon="📅" 
+                    text="親子放電區" 
+                    active={activeTab === 'playground'} 
+                />
+            </button>
         </div>
     );
 }
