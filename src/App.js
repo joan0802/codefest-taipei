95cfc85091c6ee811df59d0004bd3294e02ce8a1
YI-CHEN\sunny <sunny930817@gmail.com>
2024-09-07 18:35:54 +0800

    feat: Add loading data, change map icon according to the score of care centers.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './Navbar';
 import { useState } from 'react';
+import MapComponent from "./components/MapComponent";
 
 export default function App() {
   const [activeTab, setActiveTab] = useState('map');
@@ -13,7 +14,9 @@ export default function App() {
         </h1>
         {/* 根據 activeTab 渲染不同內容 */}
         <div className="content-container">
-          {activeTab === 'map' && <div className="map-container">地圖功能顯示在這裡</div>}
+          {activeTab === 'map' && <div className="map-container">
+            <MapComponent />
+          </div>}
           {activeTab === 'chatbot' && <div className="chatbot-container">聊天機器人顯示在這裡</div>}
           {activeTab === 'playground' && <div className="playground-container">兒童遊樂場顯示在這裡</div>}
         </div>
--- src/index.js
+++ src/index.js
@@ -3,20 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import MapComponent from './MapComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <div className="App">
-      <h1>台北育兒中心地圖</h1>
-      <MapComponent />
-    </div>
   </React.StrictMode>
 
-  
-  
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
